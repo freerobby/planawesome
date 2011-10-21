018fd6e5ecdd53214f8310ba6803a19a59850a97
Robby Grossman <robby@freerobby.com>
2011-10-21 17:59:17 -0400

    Initial commit of rails3 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planawesome::Application.config.secret_token = 'e5d30855edaf9e72df8c990ba14fcb5e2d4b68e10147ab5cfc67e04fd52361e8dfa97b378b60972e1ce0289de0236c0ec7818544dac388dd7f39a1c476b2e623'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Planawesome::Application.config.session_store :cookie_store, key: '_planawesome_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Planawesome::Application.config.session_store :active_record_store
